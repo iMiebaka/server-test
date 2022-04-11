f25e2bb6aefcb07f07b502a0048811345f642652
imiebaka <triple07even@gmail.com>
2022-04-11 22:19:36 +0100

    add readme

--- config.sh
+++ config.sh
@@ -2,9 +2,10 @@ echo "setting up enviroment"
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
-cp site-config/server-text.service /etc/systemd/system/myproject.service
+cp site-config/server.config /etc/systemd/system/myproject.service
 cp site-config/nginx.config /etc/nginx/sites-available/myproject.service
-sudo ln -s /etc/nginx/sites-available/myproject.service /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo unlink /etc/nginx/sites-enabled/default
 echo "enabling web service"
 sudo systemctl start myproject
 sudo systemctl enable myproject
