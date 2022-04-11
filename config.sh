04e8f6ef1ed6d72cac103d5ee5427672f3ec5c52
imiebaka <triple07even@gmail.com>
2022-04-11 17:57:18 +0100

    nginx fixes

--- config.sh
+++ config.sh
@@ -1,4 +1,9 @@
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
-cp server-text.service /etc/systemd/system/myproject.service
+cp site-config/server-text.service /etc/systemd/system/myproject.service
+cp site-config/nginx.config /etc/nginx/site-available/myproject.service
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+echo "restating nginx"
+sudo nginx -t
+sudo systemctl retart nginx.service
