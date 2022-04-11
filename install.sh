41e31824b006dc1bf2da11d1e2c2ffe352816b9e
imiebaka <triple07even@gmail.com>
2022-04-11 17:02:48 +0100

    add ons

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
-sudo apt-get install python3.8 -y python3-venv python3-pip python3-dev
+sudo apt-get install python3.8 -y python3-venv python3-pip python3-dev wget tree
