938d5de7c72daef993a5f25dad8c320bbc589fcc
imiebaka <triple07even@gmail.com>
2022-04-11 18:30:16 +0100

    adding fixes

--- config.sh
+++ config.sh
@@ -1,9 +1,13 @@
+echo "setting up enviroment"
 python3 -m venv venv
 . venv/bin/activate
 pip install -r requirements.txt
 cp site-config/server-text.service /etc/systemd/system/myproject.service
-cp site-config/nginx.config /etc/nginx/site-available/myproject.service
-sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+cp site-config/nginx.config /etc/nginx/sites-available/myproject.service
+sudo ln -s /etc/nginx/sites-available/myproject.service /etc/nginx/sites-enabled
+echo "enabling web service"
+sudo systemctl start myproject
+sudo systemctl enable myproject
 echo "restating nginx"
 sudo nginx -t
-sudo systemctl retart nginx.service
+sudo systemctl restart nginx.service
